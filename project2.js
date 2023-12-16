bde6d6e2fcc8d1b13320c36f092131072ff63bbc
SANDEEP1676 <19474sandeep.s@gmail.com>
2023-12-16 16:47:44 +0530

    Added new files

---
+++ project2.js
@@ -0,0 +1,15 @@
+let openBtn = document.getElementById('open-button');
+let modal = document.getElementById('modal-container');
+let closeBtn = document.getElementById('close-btn');
+openBtn.addEventListener('click',function(){
+    modal.style.display = 'block';
+});
+closeBtn.addEventListener('click',function(){
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click',function(e){
+    if(e.target == modal){
+    modal.style.display = 'none';
+    }
+});
